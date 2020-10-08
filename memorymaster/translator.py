38ab906c3c3b13cdaab8baacdc0592cc9aa769c2
suolongga <mglslg@qq.com>
2020-10-09 00:27:18 +0800

    abc

--- memorymaster/translator.py
+++ memorymaster/translator.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 
 def translate(word):
+    result = []
     url = 'http://dict.youdao.com/search'
     data = {
         'q': word,
@@ -11,8 +12,12 @@ def translate(word):
     html = requests.post(url, data=data).content
     content = str(html, 'utf-8')
     soup = BeautifulSoup(content, 'lxml')
-    translated = soup.find('div', {"class": "trans-container"}).findAll('li')
-    result = []
+    trans_container = soup.find('div', {"class": "trans-container"})
+
+    if trans_container is None:
+        return result
+
+    translated = trans_container.findAll('li')
     for x in translated:
         result.append(x.text)
 
@@ -20,5 +25,7 @@ def translate(word):
 
 
 if __name__ == '__main__':
-    rs = translate('translate')
-    print(rs[0])
+    rs = translate('transladfsdfte')
+    if len(rs) != 0:
+        print(rs[0])
+
--- memorymaster/words.py
+++ memorymaster/words.py
@@ -7,14 +7,55 @@ import translator
 _data_file = os.getcwd() + '/words_data.txt'
 
 
+def key_exists(key):
+    with open(_data_file, 'rt', encoding='utf-8') as f:
+        for line in f:
+            curr_data = json.loads(line)
+            if key == curr_data['key']:
+                return True
+
+
 def add():
-    print("请输入英文:")
-    en = input()
-    print("请输入中文:")
-    cn = input()
+    while True:
+        en = input("请输入英文:")
+        if en == 'EOF':
+            break
+
+        if key_exists(en):
+            print("当前单词已存在")
+            continue
+
+        cn = translator.translate(en)
+        if len(cn) == 0:
+            print("单词拼写错误")
+            continue
+
+        print(cn)
+
+        data = {
+            'key': en,
+            'content': cn,
+            'tags': []
+        }
+        with open(_data_file, 'at', encoding='utf-8') as f:
+            f.write(json.dumps(data, ensure_ascii=False) + '\n')
+
+
+def add_word(word):
+    if key_exists(word):
+        print("当前单词已存在")
+        return
+
+    cn = translator.translate(word)
+    if len(cn) == 0:
+        print("单词拼写错误")
+        return
+
+    print(cn)
+
     data = {
-        'en': en,
-        'cn': cn,
+        'key': word,
+        'content': cn,
         'tags': []
     }
     with open(_data_file, 'at', encoding='utf-8') as f:
@@ -24,40 +65,63 @@ def add():
 def ls():
     with open(_data_file, 'rt', encoding='utf-8') as f:
         for line in f:
-            currData = json.loads(line)
-            print(currData['cn'])
+            curr_data = json.loads(line)
+            print(curr_data)
 
 
 def ls_word(word):
     with open(_data_file, 'rt', encoding='utf-8') as f:
         for line in f:
-            currData = json.loads(line)
-            if currData['en'] == word:
-                return currData
+            curr_data = json.loads(line)
+            if curr_data['key'] == word:
+                print(curr_data)
 
 
-while True:
-    string = str(input("请输入一段要翻译的文字："))
-    result = translator.translate(string)
-    for x in result:
-        print(x)
-
-    cmd = input(">>> ")
-    if cmd == 'exit':
-        break
-    if cmd == 'add':
-        add()
-    elif cmd == 'ls':
-        ls()
+def exe_cmd_2param(command, fn):
+    results = re.split("\\s+", command)
+    if len(results) != 3:
+        print("param error")
     else:
-        w_match = re.search("ls -w\\s+\\w+", cmd)
-        if w_match:
-            results = re.split("\\s+", cmd)
-            print(len(results))
-            if len(results) != 3:
-                print("param error")
+        target_word = results[2]
+        fn(target_word)
+
+
+def start():
+    curr_words = []
+    with open(_data_file, 'rt', encoding='utf-8') as f:
+        for line in f:
+            curr_data = json.loads(line)
+            curr_words.append(curr_data)
+
+    for x in curr_words:
+        print(x['content'])
+        for i in range(0, 3):
+            input_word = input('第' + str(i) + '次:')
+            if x['key'] == input_word:
+                continue
             else:
-                target_word = results[2]
-                print(target_word)
+                i = 0
+                print('lalalalala', i)
+                continue
+
+
+if __name__ == '__main__':
+    while True:
+        cmd = input(">>> ")
+        if cmd == 'exit':
+            break
+        if cmd == 'add':
+            add()
+        elif cmd == 'ls':
+            ls()
+        elif cmd == 'start':
+            start()
         else:
-            print("command not found: " + cmd)
+            ls_w_match = re.search("ls -w\\s+\\w+", cmd)
+            add_w_match = re.search("add -w\\s+\\w+", cmd)
+            if ls_w_match:
+                exe_cmd_2param(cmd, ls_word)
+            elif add_w_match:
+                exe_cmd_2param(cmd, add_word)
+            else:
+                print("command not found: " + cmd)
