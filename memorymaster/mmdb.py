40d6f1f20d094af42787c72c37ea4995b7e7c695
索龙嘎 <suolongga@huli.com>
2020-10-20 18:02:43 +0800

    123

--- memorymaster/mmdb.py
+++ memorymaster/mmdb.py
@@ -5,10 +5,6 @@ database = MySQLDatabase('memory_master',
                             'port': 3306, 'user': 'slg', 'password': 'enter'})
 
 
-class UnknownField(object):
-    def __init__(self, *_, **__): pass
-
-
 class BaseModel(Model):
     class Meta:
         database = database
