3f637c5795e450c6c036ea6c28c984364e86ef3f
索龙嘎 <suolongga@huli.com>
2020-10-30 16:27:11 +0800

    123123

--- text/text_processor.py
+++ text/text_processor.py
@@ -1,17 +1,13 @@
 import os
 
-_data_file = os.getcwd() + '/temp.txt'
+_data_file = os.getcwd() + '/temp1.txt'
 
 
 def print_user_pic():
     with open(_data_file, 'rt', encoding='utf-8') as f:
         for line in f:
             row = list(line.split())
-            if len(row) == 3:
-                print('cnMap.put("' + row[1] + '","' + row[2] + '");')
-            else:
-                if len(row) != 0:
-                    print(row)
+            print(row[1], row[2])
 
 
 if __name__ == '__main__':
